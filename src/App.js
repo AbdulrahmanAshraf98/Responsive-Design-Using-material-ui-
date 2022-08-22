07fbdacc9ab63a4ac069fea530763165561b52c1
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-08-22 15:09:48 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Box, Stack } from "@mui/material";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
-	return <Navbar />;
+	return (
+		<>
+			<Navbar />
+			<Box>
+				<Stack direction="row" spacing={2} justify="space-between">
+					<Sidebar />
+				</Stack>
+			</Box>
+		</>
+	);
 }
 
 export default App;
